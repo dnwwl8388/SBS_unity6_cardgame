9cfc2bd2f1056954e8f68e6430afba3c8fcde50d
dnwwl8388 <dnwwl8388@naver.com>
2023-04-24 19:52:26 +0900

    dnwwl8388
    Sutda

---
+++ Sutda/ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+
+
+/////////index num[0    1    2    3    4    5    6    7    8    9]
+///////////////////1    2    3    4    5    6    7    8    9   10
+string[] deck = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                  "A", "B", "C", "B", "E", "F", "G", "H", "I", "J",};
+
+Dictionary<string, int> jokbo = new Dictionary<string, int>()
+{   //dictionary에 10땡 까지 추가하기
+
+    {"aA", 200 },
+};
+
+//List 사용해서 player0 만들고, 카드 2장 넣어보기.
+
+List<string> player0 = new List<string>();
+List<string> computer1 = new List<string>();
+
+
+player0.Add(deck[0]);
+player0.Add(deck[10]);
+
+
+computer1.Add(deck[3]);
+computer1.Add(deck[9]);
+
+
+
+player0.Sort();
+
+"aA",= 100;
+
+string player0_value = player0[0] + player0[1];
+Console.WriteLine(player
+
+Console.WriteLine
---
+++ Sutda/Sutda/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/////////index num[0    1    2    3    4    5    6    7    8    9]
+///////////////////1    2    3    4    5    6    7    8    9   10
+string[] deck = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+                  "A", "B", "C", "B", "E", "F", "G", "H", "I", "J",};
+
+
+
+//List 사용해서 playrr0 만들고, 카드 2장 넣어보기.
+List<string> player0 = new List<string>();
+List<string> computer1 = new List<string>();
+
+player0.Add(deck[0]);
+player0.Add(deck[10]);
+
+computer1.Add(deck[3]);
+computer1.Add(deck[9]);
+
+Console.WriteLine(player0[0]);
+Console.WriteLine(player0[1]);
+
+Console.WriteLine(computer1[0]);
+Console.WriteLine(computer1[1]);
+
+// A a=1000 aA = 1000
